e48e1a5df5d7a46312f291814d3469eb74d881b5
kawkawa <housni.kawtar20@gmail.com>
2023-10-06 15:17:26 +0200

    make modifictation

---
+++ project_first/password.py
@@ -0,0 +1,9 @@
+
+
+def is_acceptable_password(password: str) -> bool:
+    if( len(password) > 6 and any(char.isdigit() for char in password) and password.isdigit()==False):
+        return True
+    elif len(password) > 9:
+        return True
+    else:
+        return False
---
+++ tests/test_password.py
@@ -0,0 +1,21 @@
+from project_first import password as p
+
+
+def test_password():
+    print("Example:")
+    print(p.is_acceptable_password("short"))
+
+    # These "asserts" are used for self-checking
+    assert p.is_acceptable_password("short") == False
+    assert p.is_acceptable_password("short54") == True
+    assert p.is_acceptable_password("muchlonger") == True
+    assert p.is_acceptable_password("ashort") == False
+    assert p.is_acceptable_password("muchlonger5") == True
+    assert p.is_acceptable_password("sh5") == False
+    assert p.is_acceptable_password("1234567") == False
+    assert p.is_acceptable_password("12345678910") == True
+    assert p.is_acceptable_password("password12345") == True
+    assert p.is_acceptable_password("PASSWORD12345") == True
+    assert p.is_acceptable_password("pass1234word") == True
+
+    print("The mission is done! Click 'Check Solution' to earn rewards!")
