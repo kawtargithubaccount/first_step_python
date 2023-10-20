d2508ee53d2abfa3a86877b992d794c1fcb5be3d
kawkawa <housni.kawtar20@gmail.com>
2023-10-20 08:59:05 +0200

    modification au password

--- project_first/password.py
+++ project_first/password.py
@@ -1,17 +1,15 @@
-
-
 def is_acceptable_password(password: str) -> bool:
-    if( len(password) > 6 and any(char.isdigit() for char in password) and password.isdigit()==False):
+    if(len(password)>6 and 
+       any(char.isdigit() for char in password) 
+       and password.isdigit() is False):
         return True
     elif len(password) > 9:
         return True
     else:
         return False
-    
 def print_password(password: str)->bool:
-    if(len(password)>5):
+    if len(password)>5:
         print(password)
-        return True
-    else: 
-        print("small password ")
-        return False
+        return True 
+    print("small password ")
+    return False
