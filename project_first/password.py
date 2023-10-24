bcaec262cada9b8a6288272b623e2230ef48c757
kawkawa <housni.kawtar20@gmail.com>
2023-10-24 14:07:36 +0200

    new modif

--- project_first/password.py
+++ project_first/password.py
@@ -1,15 +1,18 @@
-def is_acceptable_password(password: str) -> bool:
-    if(len(password)>6 and 
-       any(char.isdigit() for char in password) 
-       and password.isdigit() is False):
+''' those function just for test a password '''
+def is_acceptable_password(password:str)->bool:
+    '''this function is acceptable_password returns false/true
+if the password is bigest than 6 and contain a char and it's not number'''
+    if(len(password)>6 and
+       any(char.isdigit() for char in password) and
+       password.isdigit() is False):
         return True
-    elif len(password) > 9:
+    if len(password) > 9:
         return True
-    else:
-        return False
+    return False
 def print_password(password: str)->bool:
+    '''this function print password return true/false if the password biggest than 5'''
     if len(password)>5:
         print(password)
-        return True 
+        return True
     print("small password ")
     return False
